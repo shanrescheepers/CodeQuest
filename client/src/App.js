37f0983e7e75b499705ce0e832c06a948bffc7e1
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-09-15 21:40:39 +0200

    FT mongodb mongoose nodemon dotenv

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TopNavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<AdminLogin />} />
+          <Route path="/adminmanager" element={<AdminPage />} />
+          <Route path="/" element={<Homepage />} />
+          {/* {/* <Route path="/printshop" element={<Homepage />} /> */}
+          <Route path="/printshop" element={<ShopImages />} />
+          <Route path="/productpage" element={<ProductPage />} />
+          <Route path="/theartists" element={<Artists />} />
+          <Route path="/cartpage" element={<CartPage />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
