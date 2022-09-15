b6168678af457663ff19400f8f367f6bd4df8de7
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-09-15 21:56:39 +0200

    FT Examples Added Serverside

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+// const productRoute = require('./routes/product');
+// const artistRoute = require('./routes/artist');
+// const userRoute = require('./routes/user');
+// const ordersRoute = require('./routes/orders');
+require('dotenv/config');
+
+const app = express();
+// connect hier met ander localhost
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(productRoute);
+// app.use(artistRoute);
+// app.use(userRoute);
+// app.use(ordersRoute);
+
+// mongoose.connect(process.env.DB_CONNECTION, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     dbName: 'wildlife',
+// }).then(() => console.log("Connected to wildlife DB"))
+//     .catch((err) => {
+//         console.log("No Connection. Reason: " + err);
+//     });
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => { console.log(`Server started on port: ${PORT}`) });
---
+++ server/routes/user.js
@@ -0,0 +1,20 @@
+// EXAMPLE
+// const express = require('express');
+// const router = express();
+// const { User } = require('../models/user');
+
+
+// router.get('/login', async (req, res) => {
+
+//     await User.findOne({
+//         email: req.body.email,
+//         password: req.body.password,
+//     })
+//         .then(user => res.json(user))
+//         .catch(error => res.status(500).json(error));
+// });
+
+// module.exports = router;
+
+
+// GETS POSTS UPDATE DELETES
