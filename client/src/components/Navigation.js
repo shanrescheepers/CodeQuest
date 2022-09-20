a26bc007b81db05b03168f5216fbef36223bc298
Alexa_Pettitt <91113965+AlexaPea@users.noreply.github.com>
2022-09-20 14:37:21 +0200

    Completed Login Page front end
    Here I completed all the css of the login page.

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -150,19 +150,6 @@ const Navigation = () => {
         </div>
 
 
-        {/* <ul>
-                        <li><div className="logo">SKY SKATES.</div></li>
-                        <div className="list">
-                        <li><NavLink exact activeclassname="active" to="/Home"><button className="icon icon1">Home</button></NavLink></li>
-                            <li><NavLink activeclassname="active" to="/Shop"><button className="icon icon2">Shop</button></NavLink></li>
-                            <li><NavLink activeclassname="active" to="/Admin"><button className="icon icon3">Admin</button></NavLink></li>
-                            <li><NavLink activeclassname="active" to="/Cart"><button className="icon icon3"><UilShoppingCart size="18"/></button></NavLink></li>
-                            <li><NavLink activeclassname="active" to="/"><button className="icon logout">Log out</button></NavLink></li>
-        
-        
-                        </div>
-                        </ul> */}
-
 
 
 
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,10 +1,75 @@
 import React from 'react';
+import '../css/Login.css';
+import loginImg from '../assets/loginImg.png';
+import TextField from '@mui/material/TextField';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Button from '@mui/material/Button';
 
 const LoginPage = () => {
+
+    
+
+    const theme = createTheme({
+        palette: {
+          primary: {
+            // Purple and green play nicely together.
+            main:'#2b2b2b',
+          },
+          secondary: {
+            // This is green.A700 as hex.
+            main: '#11cb5f',
+          },
+        },
+      });
+
     return (
-        <div>
+        <ThemeProvider theme={theme}>
+            <div className='login-container'>
+                <h1>Hey...</h1>
+                <h1>We missed you.</h1>
+                <h4>Let's make some magic.</h4>
+
+                <TextField sx={{
+                    backgroundColor: '#ffffff',
+                    border: 'none',
+                    outlineColor: '#ffffff',
+                    borderRadius: '30px',
+                    width: '100%',
+                    height: '50px',
+                    marginTop: '30px'
+                    
+                }}
+                id="outlined-basic" color='primary' label="Email Address" variant="outlined" />
+
+                <TextField sx={{
+                    backgroundColor: '#ffffff',
+                    border: '0',
+                    outline: '0',
+                    borderRadius: '30px',
+                    width: '100%',
+                    height: '50px',
+                    marginTop: '30px',
+                    borderBlock: 'none',
+                    borderBlockColor: '#f1f1f1'
+                }}
+                id="outlined-basic" color='primary' label="Password" variant="outlined" />
+
+                <a href='' className='link'><p>Forgot your password?</p></a>
+
+                <Button sx={{
+            backgroundColor: '#2b2b2b', borderRadius: '20px', marginTop: "20px", width: '100%', fontFamily: 'Open Sans', marginLeft: '0px',
+            '&:hover': {
+              backgroundColor: '#FF983A',
+            }
+          }} variant="contained" backgroundColor="primary">Log In</Button>
+          
+          <p className='signIn-Op'>Don't have an account?<a href=''>Sign in</a></p>
+
+            </div>
+
+            <img src={loginImg} className="loginImg"></img>
             
-        </div>
+        </ThemeProvider>
     );
 };
 
