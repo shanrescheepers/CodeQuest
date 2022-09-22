5cefcccdd75935763074fced67ff861c714f8490
cilla-thabethe <21100437@virtualwindow.co.za>
2022-09-22 02:34:10 +0200

    updated home feed page
    - created home feed css
    - imported home feed css
    - imported button component & icons from mui - created header, question feed area

--- client/src/pages/FeedPage.js
+++ client/src/pages/FeedPage.js
@@ -7,14 +7,67 @@
 //     }
 // } 
 // import Navigation from '../components/Navigation';
+
+import '../CSS/HomeFeed.css';
 import React from 'react';
-
-
+import { Button } from '@mui/material';
+import headerImg from '../Assets/homeAssets/header-img.png';
+import QuestionCard from '../components/QuestionCard';
 
 const FeedPage = () => {
+
+    const buttonStyle = {
+        backgroundColor: '#FF7900', 
+        borderRadius: '50px',
+        marginTop: '16px', 
+        width: 'auto',
+        padding: '16px 24px',
+        fontFamily: 'Open Sans',
+        textTransform: 'capitalize',
+        '&:hover': {
+            background: 'FF7900', 
+            color: '#2B2B2B'
+        }
+    }
+
+    const secondaryButtonStyle = {
+        backgroundColor: '#2B2B2B', 
+        borderRadius: '50px',
+        width: 'auto',
+        padding: '16px 24px',
+        fontFamily: 'Open Sans',
+        textTransform: 'capitalize',
+        '&:hover': {
+            background: 'FF7900', 
+            color: '#2B2B2B'
+        }
+    }
+
     return (
-        <div>
-            {/* <Navigation/> */}
+        <div className='home-con'>
+
+            <div className='feed-header'>
+                <div className='header-con'>
+                    <div className='home-header-text'>
+                        <h1>The Quest for Code Awaits...</h1>
+                        <p>Struggling with your code? Don't worry, we've all been there before. The CodeQuest community is here to help! </p>
+                        <Button variant='contained' disableElevation style={buttonStyle}>Ask a Question</Button>
+                    </div>
+                    <img className='home-header-image' src={headerImg}/>
+                </div>
+            </div>
+
+            <div className='title-con'>
+                <h2>Questions</h2>
+                <Button variant='contained' disableElevation style={secondaryButtonStyle}>Ask a Question</Button>
+            </div>
+
+            <div className='question-card-con'>
+                <QuestionCard/>
+                <QuestionCard/>
+                <QuestionCard/>
+
+            </div>
 
         </div>
     );
