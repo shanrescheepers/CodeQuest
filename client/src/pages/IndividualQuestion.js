b8f4c870cae094b2f883383243fa941b1a2d9129
Thato-Khoza <91176332+Thato-Khoza@users.noreply.github.com>
2022-09-23 13:09:39 +0200

    test
    test

--- client/src/pages/IndividualQuestion.js
+++ client/src/pages/IndividualQuestion.js
@@ -51,7 +51,7 @@ const IndividualQuestion = () => {
             dolore eu fugiat nulla pariatur.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat.
             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-            dolore eu fugiat nulla pariatur.
+            dolore eu fugiat nulla pariatur. hello world
             </p>
           </div>
 
