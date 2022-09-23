c8e6275d8f8a3ba6f04e7cb12441343f1f09f986
Alexa_Pettitt <91113965+AlexaPea@users.noreply.github.com>
2022-09-23 18:24:09 +0200

    Added login and register page
    Here I added the functionality and completed front end.

--- server/models/user.js
+++ server/models/user.js
@@ -1,39 +1,101 @@
-// EXAMPLE
-const mongoose = require('mongoose');
-const UserSchema = mongoose.Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    yearlevel: {
-        type: String,
-        required: true
-    },
-    rank: {
-        type: String,
-        required: true,
-    },
-    questionsAnswered: {
-        type: Number,
-        required: true,
-    },
-    questionsAsked: {
-        type: Number,
-        required: true,
-    },
-    profileimage: {
-        type: String,
-        required: true
+//import dependencies
+const express = require('express');
+
+//link schema
+const UserSchema = require('../models/user');
+
+//user handling dependenxies
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const router = express();
+
+
+//============================================================================================
+//Add user
+
+router.post('/api/adduser', (req, res) =>{
+
+    console.log(req.body);
+    const newUser = new UserSchema({
+        username: req.body.username, 
+        email: req.body.email, 
+        password: req.body.password, 
+        yearlevel: +req.body.yearlevel, 
+        profileimage: req.body.profileimage,
+        rank: "",
+        questionsAnswered: 0,
+        questionsAsked: 0
+
+
+    }); 
+    console.log("new user", newUser);
+
+    newUser.save()
+    .then(item => {
+        // console.log("Item log:", item);
+        res.json(item)
+    })
+    .catch(err => {
+       res.status(400).json({msg:"Can't add user, there is an error", err}); 
+    });
+});
+
+router.get('/api/getUser', async(req, res) => {
+    const user = await UserSchema.find();
+    console.log("Get Users", user);
+    res.send(user);
+});
+
+
+//============================================================================================
+//login user
+
+router.post('/api/loginUser', async (req,res) => {
+
+    const findUser = await UserSchema.findOne({
+        email: req.body.email
+    });
+
+    if(findUser){
+        if(await bcrypt.compare(req.body.password, findUser.password)){
+            const userToken = jwt.sign({
+                email: req.body.email
+            }, '883Xc7F@1dkK') //this is our secret key
+
+            return res.json({status: "Ok", user: userToken});
+
+
+        }else{
+            res.json({user: false})
+        }
+
+    }else{
+        res.json({msg: "User not found"})
+    }
+
+});
+
+
+//============================================================================================
+//encrypt password
+
+router.post('/api/verifytoken', async (req,res) =>{
+    const token = req.body.token;
+    // console.log(token);
+    const decode = jwt.verify(token, '883Xc7F@1dkK');
+
+    
+    const findUser = await UserSchema.findOne({
+        email: decode.email
+    });
+
+    if(findUser){
+        res.json({status: "ok", verified: true})
+    }else{
+        res.json({status: "bad", verified: false})
+
     }
 });
 
-const User = mongoose.model("User", UserSchema);
-module.exports = { User };
+module.exports = router;
