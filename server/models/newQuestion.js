114edf41041366f7a2191e11809b0516647ae5ec
cilla-thabethe <21100437@virtualwindow.co.za>
2022-09-29 13:59:12 +0200

    updated new question schema

--- server/models/newQuestion.js
+++ server/models/newQuestion.js
@@ -4,12 +4,12 @@ const NewQuestion = mongoose.Schema({
     userId: {type: String, required: true},
     title: {type: String, required: true},
     description: {type: String, required: true},
-    screenshots: {type: Array, required: true},
+    image: {type: String, required: true},
     code: {type: String, required: true},
     tags: {type: String, required: true},
     upvotes: {type: Number, required: true},
     downvotes: {type: Number, required: true},
-    datePosed: {type: Date, default: Date.now},
+    datePosted: {type: Date, default: Date.now},
 });
 
-module.exports = mongoose.model('question', NewQuestion)
+module.exports = mongoose.model('question', NewQuestion);
