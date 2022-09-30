2e0735ad48426f2e39c9465b891c8025a12f5e59
Simon <170044@virtualwindow.co.za>
2022-09-30 23:10:31 +0200

    Update Ui for UserProfile Page
    Added the Global Card, and updated the answer/question menu

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import '../css/profilePage.css';
 import Button from '@mui/material/Button';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import Box from '@mui/material/Box';
 
 import fishUnlockImage from '../assets/profilePageAssets/fishUnlock.png';
 import fishLockImage from '../assets/profilePageAssets/fishLock.png';
@@ -11,11 +16,34 @@ import profilePicture from '../assets/placeholderPP.png';
 import Delete from '../assets/profilePageAssets/delete.png';
 import UpVote from '../assets/profilePageAssets/upVote.png';
 import DownVote from '../assets/profilePageAssets/downVote.png';
-
-import { motion } from "framer-motion";
 import QuestionCard from '../components/QuestionCard';
 
+
+import Axios from 'axios';
+
+import { motion } from "framer-motion";
+
 const ProfilePage = () => {
+
+
+    const getUsers = () => {
+
+        // Axios.get('http://localhost:5000/api/getUser')
+        //     .then(res => {
+        //         console.log(res.data);
+        //     });
+
+
+    }
+
+
+    const [value, setValue] = React.useState('1');
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+
     return (
 
 
@@ -27,13 +55,13 @@ const ProfilePage = () => {
             <div className='pp_welcome_con'>
                 <div className='pp_welcome_banner'>
                     <h1>Hi Friend,</h1>
-                    <p>We love that you've joined us! Lets take a look at all things YOU! View your rank, badges, questions and more! </p>
+                    <p>We love that you've joined us! Lets take a look at all things YOU!</p>
                     <Button sx={{
-                        backgroundColor: '#FF7900', height: '42px',borderRadius: '20px', marginTop: "20px", width: 'auto', fontFamily: 'Open Sans', textTransform: 'capitalize',
+                        backgroundColor: '#FF7900', borderRadius: '20px', marginTop: "20px", width: '140px', fontFamily: 'Open Sans',
                         '&:hover': {
                             backgroundColor: '#FF7900',
                         }
-                    }} variant="contained">View Questions</Button>
+                    }} variant="contained">Button</Button>
                 </div>
 
                 <div className='pp_welcome_banner_img'></div>
@@ -123,21 +151,30 @@ const ProfilePage = () => {
 
 
             <div className='pp_activity_con'>
-                <p className='pp_questions_btn'>Questions</p>
-                <p className='pp_answers_btn'>Answers</p>
+                {/* <p className='pp_questions_btn'>Questions</p>
+                <p className='pp_answers_btn'>Answers</p> */}
 
-                <div className='pp_userInput_card_con'>
-                        <QuestionCard/>
-                        <QuestionCard/>
-                        <QuestionCard/>
-                    
+                <div>
+                <TabContext value={value}>
+                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
 
+                        <TabList onChange={handleChange} aria-label="lab API tabs example" >
+                            <Tab label="Answers" value="1" indicatorColor="secondary" />
+                            <Tab label="Questions" value="2" />
+                        </TabList>
+
+                    </Box>
+                    <TabPanel value="1"><QuestionCard /></TabPanel>
+                    <TabPanel value="2"><QuestionCard /></TabPanel>
+
+                </TabContext>
                 </div>
+
 
             </div>
 
-            <Button sx={{
-                backgroundColor: '#2b2b2b', float: 'right', borderRadius: '20px', height:'45px', marginTop: "150px", textTransform: 'capitalize', marginRight: "50px", width: '200px', fontFamily: 'Open Sans',
+            <Button onClick={getUsers} sx={{
+                backgroundColor: '#2b2b2b', float: 'right', borderRadius: '20px', marginTop: "150px", marginRight: "50px", width: '200px', fontFamily: 'Open Sans',
                 '&:hover': {
                     backgroundColor: '#2b2b2b',
                 }
