7a1bdf37ca90efe8392d9e81c3d3250b7c3a907c
cilla-thabethe <21100437@virtualwindow.co.za>
2022-10-04 13:41:28 +0200

    frontend image display updated

--- client/src/pages/NewQuestionPage.js
+++ client/src/pages/NewQuestionPage.js
@@ -6,8 +6,10 @@ import { alpha, styled } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
 import Axios from 'axios';
-import { useState } from 'react';
 import Helmet from "react-helmet";
+import { useEffect, useState } from 'react';
+import upload from '../assets/newQuestionAssets/upload.png';
+
 const NewQuestionPage = () => {
     
     // const CssTextField = styled(TextField)({
@@ -66,25 +68,66 @@ const NewQuestionPage = () => {
 
     //screenshot names
     const [screenshots, setScreenshots] = useState([]);
+    const [screenshotFiles, setImageFiles] = useState([]);
+    const [uploadedScreenshots, setImages] = useState([]);
+    const [index, setIndex] = useState(0);
 
-    //get screenshots
+    const imageTypeRegex = /image\/(png|jpg|jpeg)/gm;
+
+    //get screenshots + display them in the image slider
     const getScreenshots = (e) => {
-        let imageFile = e.target.files;
-        setScreenshots(imageFile);
 
-        // for(let i = 0; i < imageFile.length; i++){
-        //     console.log(imageFile[i]);
-        // }
-      
-        let reader = new FileReader();
-        reader.onload = () => {
-          let output = document.getElementById('screenshot-preview');
-          output.src = reader.result;
-        }; 
-      
-        reader.readAsDataURL(e.target.files[0]); 
+        // // hide screenshots upload thingie
+        document.getElementById('upload-image-con').style.display = 'none';
+        document.getElementById('screenshot-preview').style.backgroundColor = '#F1F1FC';
+
+        let screenshotFiles = e.target.files;
+        setScreenshots(screenshotFiles);
+
+        const { files } = e.target;
+        const validImageFiles = [];
+        for (let i = 0; i < files.length; i++) {
+          const file = files[i];
+          if (file.type.match(imageTypeRegex)) {
+            validImageFiles.push(file);
+          }
+        }
+        if (validImageFiles.length) {
+          setImageFiles(validImageFiles);
+          return;
+        }
+        alert("Selected uploadedScreenshots are not of valid type!");
     }
 
+    useEffect(() => {
+        const uploadedScreenshots = [], fileReaders = [];
+        let isCancel = false;
+        if (screenshotFiles.length) {
+          screenshotFiles.forEach((file) => {
+            const fileReader = new FileReader();
+            fileReaders.push(fileReader);
+            fileReader.onload = (e) => {
+              const { result } = e.target;
+              if (result) {
+                uploadedScreenshots.push(result)
+              }
+              if (uploadedScreenshots.length === screenshotFiles.length && !isCancel) {
+                setImages(uploadedScreenshots);
+              }
+            }
+            fileReader.readAsDataURL(file);
+          })
+        };
+    return () => {
+        isCancel = true;
+        fileReaders.forEach(fileReader => {
+          if (fileReader.readyState === 1) {
+            fileReader.abort()
+          }
+        })
+      }
+    }, [screenshotFiles]);
+ 
     //add new question to database
     const addNewQuestion = (e) => {
         e.preventDefault();
@@ -124,9 +167,11 @@ const NewQuestionPage = () => {
             downvotes: +Downvotes,
         }
 
+        //appends text 
         payloadData.append('information', JSON.stringify(payload));
+
+        //appends uploadedScreenshots
         for(let i = 0; i < screenshots.length; i++){
-            // console.log(screenshots[i]);
             const element = screenshots[i];
             payloadData.append('screenshots', element);
         }
@@ -168,7 +213,41 @@ const NewQuestionPage = () => {
 
                     <div className='screenshot-preview'>
                         <img className='screenshot' id="screenshot-preview"/>
+
+                        <div className='screenshot-preview' id='screenshot-preview'>
+                            <div className='upload-image-con' id='upload-image-con'>
+                                <div className='things'>
+                                    <img className='img-picture' src={upload} alt='image placeholder'/>
+                                    <p>Click here to add your screenshots.</p>
+                                </div>
+
+                                <input name='screenshots' className='image-input' id='image-input' type="file"  accept='image/*' multiple onChange={getScreenshots}/>
+                            </div>
+
+                            <div className="slideshow">
+                                <div className="slideshow-slider" style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}>
+                                    {uploadedScreenshots.map((screenshot, index) => (
+                                        <div className="slide" key={index}>
+                                            <img src={screenshot} className="slide-img"/>
+                                        </div>
+                                    ))}
+                                </div>
+
+                                <div className="slideshow-dots">
+                                    {uploadedScreenshots.map((_, idx) => (
+                                        <div key={idx} className={`slideshow-dot${index === idx ? " active" : ""}`}
+                                            onClick={() => {
+                                                setIndex(idx);
+                                            }}>    
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+
+                        </div>
+
                     </div>
+                    
 
                     <TextField name='code' placeholder='Code' color='grey' multiline fullWidth sx={{backgroundColor: 'white', borderRadius: '50px', marginTop: '16px'}} onChange={getFormValues}/>
                     <TextField name='tags' placeholder='Tags' color='grey' fullWidth sx={{backgroundColor: 'white', borderRadius: '50px', marginTop: '16px'}} onChange={getFormValues}/>
--- server/models/votes.js
+++ server/models/votes.js
@@ -1,20 +1,11 @@
 // EXAMPLE
 const mongoose = require('mongoose');
 const VotesSchema = mongoose.Schema({
-    vote: {
-        type: Number,
-        required: true
-    },
-
-    userId: {
-        type: String,
-        required: true
-    },
-    postId: {
-        type: String,
-        required: true
-    }
+    vote: { type: Number, required: true },
+    userId: { type: String, required: true },
+    questionId: { type: String, required: true }
 });
+
 // upvote = 1. downvote = 0.
 const Votes = mongoose.model("Votes", VotesSchema);
 module.exports = { User };
