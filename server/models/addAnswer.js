ba63dca7bd4897bb890a42263f85282f4744b600
cilla-thabethe <21100437@virtualwindow.co.za>
2022-10-17 05:37:27 +0600

    updated answer + question schema
    changed question + answer schema so that image uploads aren't compulsory.

--- server/models/addAnswer.js
+++ server/models/addAnswer.js
@@ -4,7 +4,7 @@ const addAnswer = mongoose.Schema({
     userId: {type: String, required: true},
     questionId: {type: String, required: true},
     description: {type: String, required: true},
-    screenshots: {type: Array, required: true},
+    screenshots: {type: Array, required: false},
     code: {type: String, required: true},
     upvotes: {type: Number, required: true},
     downvotes: {type: Number, required: true},
