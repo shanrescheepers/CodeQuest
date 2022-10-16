d26f1a41ff9ec0ba85dba4e6da53573bb7cb1812
Alexa_Pettitt <91113965+AlexaPea@users.noreply.github.com>
2022-10-16 23:10:42 +0200

    Added voting functionality

--- client/src/components/QuestionCard.js
+++ client/src/components/QuestionCard.js
@@ -28,83 +28,363 @@ const QuestionCard = (props) => {
         setFlagModal(<FlagModal close={setFlagModal} id={props.id} questionId={props.questionId} userId={props.userId} />)
     }
 
+
+    //=====================================================================
+//Get Current Vote state
+
+const [arrowImgUp, setArrowImgUp] = useState('Up');
+const [arrowImgDown, setArrowImgDown] = useState('Down');
+const [category, setCategory] = useState('');
+
+useEffect(() => {
+      axios.get('http://localhost:5000/api/readvote')
+        .then(res => {
+          let data = res.data;
+         console.log(data);
+        let user = sessionStorage.getItem('id');
+
+        for(let i=0; i<data.length;i++){
+            if(props.questionId === data[i].questionId){
+
+                console.log(user, data[i].userId);
+                if(user === data[i].userId){
+                   if(data[i].vote === 'upvote'){
+                       setArrowImgUp('UpActive');
+                       setArrowImgDown('Down')
+                       setCategory('startUp')
+                   }else if(data[i].vote === 'downvote'){
+                       setArrowImgDown('DownActive')
+                       setArrowImgUp('Up');
+                       setCategory('startDown')
+                };
+                console.log("not happening");
+                
+               }else{
+                   console.log('N/A');
+               }
+
+
+            }else{
+                // console.log('not relevent');
+            }
+
+        }
+
+        })
+         .catch(function (error) {
+            console.log(error);
+        });
+
+    }, []);
+  
+
+
+
+//=====================================================================
+//Upvote and downvote
+
+let upVotes = props.upvotes;
+let downVotes = props.downvotes;
+
+const [voteCast, setVoteCast] = useState('');
+const [upVoteCast, setUpvoteCast] = useState(props.upvotes);
+const [downVoteCast, setDownvoteCast] = useState(props.downvotes);
+
+
+  const handleVote = (event, vote) => {
+    setVoteCast(vote);
+    console.log(vote);
+
+    if(category === 'startUp' && arrowImgUp === 'UpActive' && arrowImgDown === 'Down'){
+            if(vote ==='up'){
+                setUpvoteCast(props.upvotes -1);
+                setDownvoteCast(props.downvotes);
+                setArrowImgUp('Up');
+                setArrowImgDown('Down');
+            }else if(vote ==='down'){
+                setDownvoteCast(props.downvotes +1); 
+                setUpvoteCast(props.upvotes-1);
+                setArrowImgUp('Up');
+                setArrowImgDown('DownActive'); 
+            }
+        };
+        if(category === 'startUp' && arrowImgUp === 'Up' && arrowImgDown === 'Down'){
+            if(vote ==='up'){
+                setUpvoteCast(props.upvotes);
+                setDownvoteCast(props.downvotes);
+                setArrowImgUp('UpActive');
+                setArrowImgDown('Down');
+            }else if(vote ==='down'){
+                setDownvoteCast(props.downvotes +1); 
+                setUpvoteCast(props.upvotes-1);
+                setArrowImgUp('Up');
+                setArrowImgDown('DownActive'); 
+            }
+        };
+         if(category === 'startUp' && arrowImgUp === 'Up' && arrowImgDown === 'DownActive'){
+                if(vote ==='up'){
+                    setUpvoteCast(props.upvotes);
+                    setDownvoteCast(props.downvotes);
+                    setArrowImgUp('UpActive');
+                    setArrowImgDown('Down');
+                }else if(vote ==='down'){
+                    setDownvoteCast(props.downvotes); 
+                    setUpvoteCast(props.upvotes-1);
+                    setArrowImgUp('Up');
+                    setArrowImgDown('Down'); 
+                }
+            };
+
+            //start down
+
+            if(category === 'startDown' && arrowImgUp === 'Up' && arrowImgDown === 'Down'){
+                if(vote ==='up'){
+                    setUpvoteCast(props.upvotes+1);
+                    setDownvoteCast(props.downvotes-1);
+                    setArrowImgUp('UpActive');
+                    setArrowImgDown('Down');
+                }else if(vote ==='down'){
+                    setDownvoteCast(props.downvotes); 
+                    setUpvoteCast(props.upvotes);
+                    setArrowImgUp('Up');
+                    setArrowImgDown('DownActive'); 
+                }
+            };
+
+            if(category === 'startDown' && arrowImgUp === 'UpActive' && arrowImgDown === 'Down'){
+                if(vote ==='up'){
+                    setUpvoteCast(props.upvotes-1);
+                    setDownvoteCast(props.downvotes-1);
+                    setArrowImgUp('Up');
+                    setArrowImgDown('Down');
+                    console.log('active to nuet');
+                }else if(vote ==='down'){
+                    setDownvoteCast(props.downvotes); 
+                    setUpvoteCast(props.upvotes);
+                    setArrowImgUp('Up');
+                    setArrowImgDown('DownActive'); 
+                }
+            };
+
+            //start down
+
+             if(category === 'startDown' && arrowImgUp === 'Up' && arrowImgDown === 'DownActive'){
+                    if(vote ==='up'){
+                        setUpvoteCast(props.upvotes+1);
+                        setDownvoteCast(props.downvotes-1);
+                        setArrowImgUp('UpActive');
+                        setArrowImgDown('Down');
+                    }else if(vote ==='down'){
+                        setDownvoteCast(props.downvotes-1); 
+                        setUpvoteCast(props.upvotes);
+                        setArrowImgUp('Up');
+                        setArrowImgDown('Down'); 
+                    }
+                };
+
+            if(category === 'startDown' && arrowImgUp === 'Up' && arrowImgDown === 'Down'){
+                if(vote ==='up'){
+                    setUpvoteCast(props.upvotes+1);
+                    setDownvoteCast(props.downvotes-1);
+                    setArrowImgUp('UpActive');
+                    setArrowImgDown('Down');
+                }else if(vote ==='down'){
+                    setDownvoteCast(props.downvotes); 
+                    setUpvoteCast(props.upvotes);
+                    setArrowImgUp('Up');
+                    setArrowImgDown('DownActive'); 
+                }
+            };
+
+            if(category === 'startDown' && arrowImgUp === 'UpActive' && arrowImgDown === 'Down'){
+                if(vote ==='up'){
+                    setUpvoteCast(props.upvotes-1);
+                    setDownvoteCast(props.downvotes-1);
+                    setArrowImgUp('Up');
+                    setArrowImgDown('Down');
+                    console.log('active to nuet');
+                }else if(vote ==='down'){
+                    setDownvoteCast(props.downvotes); 
+                    setUpvoteCast(props.upvotes);
+                    setArrowImgUp('Up');
+                    setArrowImgDown('DownActive'); 
+                }
+            };
+
+            //start neutral
+
+        if(category === '' && arrowImgUp === 'Up' && arrowImgDown === 'Down'){
+            if(vote ==='up'){
+                setUpvoteCast(props.upvotes+1);
+                setDownvoteCast(props.downvotes);
+                setArrowImgUp('UpActive');
+                setArrowImgDown('Down');
+            }else if(vote ==='down'){
+                setDownvoteCast(props.downvotes+1); 
+                setUpvoteCast(props.upvotes);
+                setArrowImgUp('Up');
+                setArrowImgDown('DownActive'); 
+            }
+        };
+
+        if(category === '' && arrowImgUp === 'UpActive' && arrowImgDown === 'Down'){
+            if(vote ==='up'){
+                setUpvoteCast(props.upvotes);
+                setDownvoteCast(props.downvotes);
+                setArrowImgUp('Up');
+                setArrowImgDown('Down');
+                console.log('active to nuet');
+            }else if(vote ==='down'){
+                setDownvoteCast(props.downvotes+1); 
+                setUpvoteCast(props.upvotes);
+                setArrowImgUp('Up');
+                setArrowImgDown('DownActive'); 
+            }
+        };
+
+             if(category === '' && arrowImgUp === 'Up' && arrowImgDown === 'DownActive'){
+                    if(vote ==='up'){
+                        setUpvoteCast(props.upvotes+1);
+                        setDownvoteCast(props.downvotes);
+                        setArrowImgUp('UpActive');
+                        setArrowImgDown('Down');
+                    }else if(vote ==='down'){
+                        setDownvoteCast(props.downvotes); 
+                        setUpvoteCast(props.upvotes);
+                        setArrowImgUp('Up');
+                        setArrowImgDown('Down'); 
+                    }
+                };
+        
+   
+  
+};
+  //format votes
+
+  let displayUpVote = upVoteCast;
+  if(displayUpVote>9){
+    displayUpVote = displayUpVote
+  }else{
+    displayUpVote = '0' + displayUpVote
+  }
+
+  let displayDownVote = downVoteCast;
+  if(displayDownVote>9){
+    displayDownVote = displayDownVote
+  }else{
+    displayDownVote = '0' + displayDownVote
+  }
+ 
+
+//UpVote
     const addVote = () => {
-        //add vote functionality
-    }
+        // console.log("It works, Whoopieee");
+
+        let payloadData={
+            vote: 'upvote',
+            userId: sessionStorage.getItem('id'),
+            questionId: props.questionId
+        }
+
+        axios.post('http://localhost:5000/api/addvote', payloadData)
+        .then((res)=> {
+            if(res){
+            // console.log("Vote Added"); 
+            console.log(payloadData);
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+    };
+
+//======================================================================
+//downvote
+
+    const [downClickVote, setDownClickVote] = useState(props.downvotes);
 
     const subtractVote = () => {
-        //subtract vote functionality
+        // console.log("It works, Whoopieee");
+
+        let payloadData={
+            vote: 'downvote',
+            userId: sessionStorage.getItem('id'),
+            questionId: props.questionId
+        }
+        // console.log(payloadData);
+
+        axios.post('http://localhost:5000/api/addvote', payloadData)
+        .then((res)=> {
+            if(res){
+            console.log("Vote Added"); 
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
     }
 
-    //===================================================================
-    //Format Date date
+//===================================================================
+//Format Date date
     let date = props.date;
     let formatDate = moment(date).format('DD MMMM YYYY');
 
 
-    //====================================================================
-    //Cut descirption
+ //====================================================================
+//Cut descirption
 
-    let desc = (props.description).substring(0, 80);
+    let desc = (props.description).substring(0,80);
 
 
-    //====================================================================
-    //Format votes
 
-    let upVotes = props.upvotes;
-    if (upVotes > 9) {
-        upVotes = props.upvotes;
-    } else {
-        upVotes = '0' + upVotes;
-    }
-
-    let downVotes = props.downvotes;
-    if (upVotes > 9) {
-        downVotes = props.downvotes;
-    } else {
-        downVotes = '0' + downVotes;
-    }
-    //=====================================================================
-    //User Info
+//=====================================================================
+//User Info
 
     const [username, setUsername] = useState();
     const [rank, setRank] = useState();
     const [profileImg, setprofileImg] = useState();
     const [year, setYear] = useState();
 
-    useEffect(() => {
+useEffect(() => {
 
-        if (props.userId == null) {
-            // console.log("User not logged in")
+  if (props.userId == null) {
+    console.log("User not logged in")
 
-        } else {
-            // console.log("user logged in")
-            axios.get('http://localhost:5000/api/userInfo/' + props.userId)
-                .then(res => {
-                    let data = res.data;
-                    setUsername(data.username);
-                    setRank(data.rank);
-                    setprofileImg(data.profileimage);
-                    setYear(data.yearlevel);
-                    // console.log(data.rank)
-                })
-        }
-        // localStorage.clear();
-    }, []);
+  } else {
+    console.log("user logged in")
+    axios.get('http://localhost:5000/api/userInfo/' + props.userId)
+      .then(res => {
+        let data = res.data;
+        setUsername(data.username);
+        setRank(data.rank);
+        setprofileImg(data.profileimage);
+        setYear(data.yearlevel);
+        console.log(data.rank)
+      })
+  }
+  // localStorage.clear();
+}, []);
 
-    //get profile image path
-    const imgURL = ('Avatars/' + profileImg + '.png');
+//get profile image path
+const imgURL = ('Avatars/' + profileImg + '.png');
 
-    // console.log(year);
-    let bgColor = '';
+console.log(year);
+let bgColor = '';
 
-    if (year === 1) {
-        bgColor = '#6EEB83'
-    } else if (year === 2) {
-        bgColor = '#6CD4FF'
-    } else {
-        bgColor = '#FF7900'
-    };
+if (year === 1) {
+  bgColor = '#6EEB83'
+} else if (year === 2) {
+  bgColor = '#6CD4FF'
+} else {
+  bgColor = '#FF7900'
+};
+
+
+const upImgURL = ('Votes/' + arrowImgUp + '.png');
+const downImgURL = ('Votes/' + arrowImgDown + '.png');
+
+
 
     return (
         <>{flagModal}
