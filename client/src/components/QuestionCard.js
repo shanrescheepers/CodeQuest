fc1a6d73e64e4d564fdac4f01a9d0d613b581704
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-10-16 15:05:03 +0200

    FT: Flag Modal + onClick

--- client/src/components/QuestionCard.js
+++ client/src/components/QuestionCard.js
@@ -8,6 +8,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import moment from 'moment';
 import { useNavigate } from 'react-router';
+import FlagModal from '../modals/FlagModal';
 
 const QuestionCard = (props) => {
 
@@ -16,13 +17,15 @@ const QuestionCard = (props) => {
     //navigate to individual question page
     const goToIndividualQuestion = () => {
         navigate('/IndividualQuestion');
-        
+
         //send question id to session storage
         sessionStorage.setItem('questionId', props.questionId);
     }
+    const [flagModal, setFlagModal] = useState()
 
-    const deleteQuestion = () => {
-        //delete question functionality
+
+    const flagQuestion = () => {
+        setFlagModal(<FlagModal close={setFlagModal} id={props.id} />)
     }
 
     const addVote = () => {
@@ -42,23 +45,23 @@ const QuestionCard = (props) => {
     //====================================================================
     //Cut descirption
 
-    let desc = (props.description).substring(0,80);
+    let desc = (props.description).substring(0, 80);
 
 
     //====================================================================
     //Format votes
 
     let upVotes = props.upvotes;
-    if(upVotes>9){
+    if (upVotes > 9) {
         upVotes = props.upvotes;
-    }else{
+    } else {
         upVotes = '0' + upVotes;
     }
 
     let downVotes = props.downvotes;
-    if(upVotes>9){
+    if (upVotes > 9) {
         downVotes = props.downvotes;
-    }else{
+    } else {
         downVotes = '0' + downVotes;
     }
     //=====================================================================
@@ -69,77 +72,80 @@ const QuestionCard = (props) => {
     const [profileImg, setprofileImg] = useState();
     const [year, setYear] = useState();
 
-useEffect(() => {
+    useEffect(() => {
 
-  if (props.userId == null) {
-    // console.log("User not logged in")
+        if (props.userId == null) {
+            // console.log("User not logged in")
 
-  } else {
-    // console.log("user logged in")
-    axios.get('http://localhost:5000/api/userInfo/' + props.userId)
-      .then(res => {
-        let data = res.data;
-        setUsername(data.username);
-        setRank(data.rank);
-        setprofileImg(data.profileimage);
-        setYear(data.yearlevel);
-        // console.log(data.rank)
-      })
-  }
-  // localStorage.clear();
-}, []);
+        } else {
+            // console.log("user logged in")
+            axios.get('http://localhost:5000/api/userInfo/' + props.userId)
+                .then(res => {
+                    let data = res.data;
+                    setUsername(data.username);
+                    setRank(data.rank);
+                    setprofileImg(data.profileimage);
+                    setYear(data.yearlevel);
+                    // console.log(data.rank)
+                })
+        }
+        // localStorage.clear();
+    }, []);
 
-//get profile image path
-const imgURL = ('Avatars/' + profileImg + '.png');
+    //get profile image path
+    const imgURL = ('Avatars/' + profileImg + '.png');
 
-// console.log(year);
-let bgColor = '';
+    // console.log(year);
+    let bgColor = '';
 
-if (year === 1) {
-  bgColor = '#6EEB83'
-} else if (year === 2) {
-  bgColor = '#6CD4FF'
-} else {
-  bgColor = '#FF7900'
-};
+    if (year === 1) {
+        bgColor = '#6EEB83'
+    } else if (year === 2) {
+        bgColor = '#6CD4FF'
+    } else {
+        bgColor = '#FF7900'
+    };
 
     return (
-        <div className='question-con' onClick={goToIndividualQuestion}>
-            <div className='question-con-content'>
-                <div className='top-block'>
-                    <div className='user-info-block'>
-                    <div className='profile-circle question' style={{ backgroundColor: bgColor }}><img src={imgURL} className="profile-Img question"></img></div>
-                        <div className='user-info'>
-                            <h4>{username}</h4>
-                            <p>{formatDate}</p>
+        <>{flagModal}
+            <div className='question-con' >
+
+                <div className='question-con-content'>
+                    <div className='top-block'>
+                        <div className='user-info-block' onClick={() => goToIndividualQuestion()}>
+                            <div className='profile-circle question' style={{ backgroundColor: bgColor }}><img src={imgURL} className="profile-Img question"></img></div>
+                            <div className='user-info'>
+                                <h4>{username}</h4>
+                                <p>{formatDate}</p>
+                            </div>
+                        </div>
+
+                        <div className='flag-button question-card-icon' onClick={() => flagQuestion()}>
+                            <OutlinedFlagIcon fontSize="large" />
                         </div>
                     </div>
 
-                    <div className='delete-button question-card-icon' onClick={deleteQuestion}>
-                    <OutlinedFlagIcon fontSize="large"/>
-                    </div>
-                </div>
-
-                <div className='user-question'>
-                    <h3>Q: {props.title}</h3>
-                    <p>{desc} ...</p>
-                </div>
-
-                <div className='divider'></div>
-
-                <div className='bottom-block'>
-                    <div className='arrow-con'>                         
-                        <img className='upvote question-card-icon' onClick={addVote} src={upvote}/>
-                        <small className='upvote-count vote-count'>{upVotes}</small>
-
-                        <img className='downvote question-card-icon' onClick={subtractVote} src={downvote}/>
-                        <small className='downvote-count vote-count'>{downVotes}</small>
+                    <div className='user-question' onClick={() => goToIndividualQuestion()}>
+                        <h3>Q: {props.title}</h3>
+                        <p>{desc} ...</p>
                     </div>
 
-                    <small><p>00 Answers</p></small>
+                    <div className='divider' onClick={() => goToIndividualQuestion()}></div>
+
+                    <div className='bottom-block'>
+                        <div className='arrow-con'>
+                            <img className='upvote question-card-icon' onClick={addVote} src={upvote} />
+                            <small className='upvote-count vote-count'>{upVotes}</small>
+
+                            <img className='downvote question-card-icon' onClick={subtractVote} src={downvote} />
+                            <small className='downvote-count vote-count'>{downVotes}</small>
+                        </div>
+
+                        <small><p>00 Answers</p></small>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
--- client/src/modals/FlagModal.js
+++ client/src/modals/FlagModal.js
@@ -7,11 +7,14 @@ import Box from "@mui/material/Box";
 import FormControl from "@mui/material/FormControl";
 import TextField from '@mui/material/TextField';
 
-const FlagModal = () => {
+const FlagModal = (props) => {
+    const closeModal = () => {
+        props.close();
+    }
     return (
-        <div className='pop-up'>
+        <div className='pop-up' style={{ marginBottom: "100px", position: "fixed" }}>
             <div className='modal deleteAccount'>
-                <div className='close'><CloseIcon sx={{fontSize:'40px'}}/></div>
+                <div className='close' onClick={closeModal}><CloseIcon sx={{ fontSize: '40px' }} /></div>
                 <h1>Flag alert.</h1>
                 <h4>Uh-Oh! What was the problem with this question?</h4>
                 <div className='modal-img delete'><img src={picture}></img></div>
@@ -20,28 +23,29 @@ const FlagModal = () => {
 
                     <FormControl fullWidth>
                         <TextField name='description' placeholder='Description' multiline color='grey' fullWidth sx={{
-                            backgroundColor: 'white', 
-                            borderRadius: '50px', 
-                            marginTop: '-5px', 
-                            marginBottom: '0px', 
-                            marginLeft: '-20px'}} />
-                    
+                            backgroundColor: 'white',
+                            borderRadius: '50px',
+                            marginTop: '-5px',
+                            marginBottom: '0px',
+                            marginLeft: '-20px'
+                        }} />
+
 
                         <Button sx={{
-                        backgroundColor: '#2b2b2b',
-                        borderRadius: '20px',
-                        marginTop: "20px", 
-                        textTransform: 'capitalize', 
-                        width: '95%', 
-                        height: '45px', 
-                        fontFamily: 'Open Sans', 
-                        marginLeft: '-15px',
-                        '&:hover': {
-                        backgroundColor: '#4A4A4A',
-                        }
-                    }} variant="contained" type="submit" backgroundColor="primary">Flag Question</Button>
-                </FormControl>
-            </Box>
+                            backgroundColor: '#2b2b2b',
+                            borderRadius: '20px',
+                            marginTop: "20px",
+                            textTransform: 'capitalize',
+                            width: '95%',
+                            height: '45px',
+                            fontFamily: 'Open Sans',
+                            marginLeft: '-15px',
+                            '&:hover': {
+                                backgroundColor: '#4A4A4A',
+                            }
+                        }} variant="contained" type="submit" backgroundColor="primary">Flag Question</Button>
+                    </FormControl>
+                </Box>
 
             </div>
         </div>
