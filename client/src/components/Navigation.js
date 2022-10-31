f6c5e12ec6fd006508257aad5ba7bc22ac53c8a5
cilla-thabethe <21100437@virtualwindow.co.za>
2022-10-31 03:26:52 +0200

    updated navigation + footer frontend

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -18,8 +18,6 @@ import LogOutModal from "../modals/LogOutModal";
 import FeedPage from "../pages/FeedPage";
 import QuestionCard from "../components/QuestionCard";
 
-// import Stack from '@mui/material/Stack';
-
 const Navigation = ({ questions, setUpdateQuestions }) => {
   // const [loggedIn, setLoggedIn] = useState(false);
   const [loggedIn, setLoggedIn] = useState(true);
@@ -32,52 +30,44 @@ const Navigation = ({ questions, setUpdateQuestions }) => {
     }
   };
 
-  const goToDiscord = () => {
-    window.open("https://discord.gg/GpqtG8dHpn", "_blank");
-  };
-
-  const Search = styled("div")(({ theme }) => ({
-    position: "relative",
-    float: "left",
-    borderRadius: "30px",
-    marginTop: "20px",
-    boxShadow: "rgba(0, 0, 0, 0.1) -4px 9px 25px -6px",
-    backgroundColor: "#FFFFFF",
-    "&:hover": {
-      backgroundColor: "#FFFFFF",
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: "30",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(1),
-      width: "25%",
-    },
-  }));
-
-  const SearchIconWrapper = styled("div")(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  }));
-
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: "inherit",
-    fontFamily: "Open Sans",
-    "& .MuiInputBase-input": {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create("width"),
-      width: "100%",
-      [theme.breakpoints.up("md")]: {
-        width: "60ch",
-      },
-    },
-  }));
+    const Search = styled('div')(({ theme }) => ({
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        borderRadius: "30px",
+        boxShadow: "rgba(0, 0, 0, 0.1) -4px 9px 25px -6px",
+        backgroundColor: "#FFFFFF",
+        "&:hover": {
+        backgroundColor: "#FFFFFF",
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: "30",
+        [theme.breakpoints.up("sm")]: {
+        marginLeft: 0,
+        width: "150%",
+        },
+    }));
+  
+    const SearchIconWrapper = styled('div')(({ theme }) => ({
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }));
+  
+    const StyledInputBase = styled(InputBase)(({ theme }) => ({
+        color: 'inherit',
+        '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        display: 'block',
+        },
+    }));
 
   //================================================================================================
   //Log out
@@ -85,6 +75,10 @@ const Navigation = ({ questions, setUpdateQuestions }) => {
 
   const logOut = () => {
     setEditModal(<LogOutModal close={setEditModal} />);
+  };
+
+  const goToDiscord = () => {
+    window.open("https://discord.gg/GpqtG8dHpn", "_blank");
   };
 
   //========================================
@@ -196,162 +190,164 @@ const Navigation = ({ questions, setUpdateQuestions }) => {
       });
     });
   };
-  return (
-    <div>
-      {editModal}
-      <div className="navContainer">
-        <div className="topNavBar">
-          <img src={logo} className="Logo" width="150px"></img>
 
-          <Search
-            style={{
-              marginLeft: "100px",
-              width: "390px",
-              height: "42px",
-              marginTop: "25px",
-              marginLeft: "15px",
-            }}
-          >
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
+    return (
+        <div>
+            {editModal}
+            <div className="navContainer">
+                <div className="top-nav">
+                    <img src={logo} className="Logo"></img>
 
-            <StyledInputBase
-              placeholder="What would you like to know?"
-              inputProps={{ "aria-label": "search" }}
-              id="search"
-            />
-          </Search>
-          <div className="nav_btn">
-            <Button
-              sx={{
-                backgroundColor: "#2b2b2b",
-                borderRadius: "20px",
-                height: "42px",
-                marginTop: "25px",
-                width: "140px",
-                fontFamily: "Open Sans",
+                    <div className="search-block">
+                        <Search>
+                            <SearchIconWrapper>
+                                <SearchIcon />
+                            </SearchIconWrapper>
 
-                "&:hover": {
-                  backgroundColor: "#4A4A4A",
-                },
-              }}
-              variant="contained"
-              backgroundColor="primary"
-              onClick={SearchBtn}
-            >
-              Search
-            </Button>
+                            <StyledInputBase 
+                            sx= {{display: 'block'}}
+                            placeholder="What would you like to know?"
+                            inputProps={{ "aria-label": "search" }}
+                            id="search"
+                            />
+                        </Search>
 
-            <Button
-              sx={{
-                backgroundColor: "#2b2b2b",
-                borderRadius: "20px",
-                height: "42px",
-                marginTop: "25px",
-                width: "140px",
-                fontFamily: "Open Sans",
-                marginRight: "75px",
-                "&:hover": {
-                  backgroundColor: "#4A4A4A",
-                },
-              }}
-              variant="contained"
-              backgroundColor="primary"
-              onClick={logOut}
-            >
-              Log Out
-            </Button>
-          </div>
-        </div>
+                        <Button
+                            sx={{
+                                backgroundColor: "#2b2b2b",
+                                borderRadius: "50px",
+                                height: "auto",
+                                width: "180px",
+                                fontFamily: "Open Sans",
 
-        <div className="sideNav">
-          <div className="profile-section">
-            <div className="profileCircle" style={{ backgroundColor: bgColor }}>
-              <img src={imgURL} className="profile-Img"></img>
+                                "&:hover": {
+                                backgroundColor: "#4A4A4A",
+                                },
+                            }}
+                            disableElevation
+                            variant="contained"
+                            backgroundColor="primary"
+                            onClick={SearchBtn}
+                            >
+                            Search
+                        </Button>
+
+                    </div>
+
+                    <Button
+                        sx={{
+                            backgroundColor: "#2b2b2b",
+                            borderRadius: "50px",
+                            height: "auto",
+                            // marginTop: "25px",
+                            width: "180px",
+                            fontFamily: "Open Sans",
+                            // marginRight: "75px",
+                            "&:hover": {
+                            backgroundColor: "#4A4A4A",
+                            },
+                        }}
+                        disableElevation
+                        size="medium"
+                        variant="contained"
+                        backgroundColor="primary"
+                        onClick={logOut}
+                        >
+                        Log Out
+                    </Button>
+                </div>
+
+                <div className="sideNav">
+                    <div className="profile-section">
+                        <div className="profileCircle" style={{ backgroundColor: bgColor }}>
+                            <img src={imgURL} className="profile-Img"></img>
+                        </div>
+
+                        <h3>{username}</h3>
+                        <h5>{rank} </h5>
+                    </div>
+
+                    <div className="Navigation">
+                        {loggedIn ? (
+                            <div className="list">
+                                <li>
+                                    <NavLink activeclassname="active" to="/FeedPage">
+                                        <button>
+                                            <HomeOutlinedIcon className='icon'/>
+                                            Feed
+                                        </button>
+                                    </NavLink>
+                                </li>
+                                
+                                <li>       
+                                    <NavLink activeclassname="active" to="/QuestionsPage">           
+                                        <button>
+                                            <QuestionAnswerOutlinedIcon className='icon'/>
+                                            Questions
+                                        </button>
+                                    </NavLink>
+                                </li>
+
+                                <li>        
+                                    <NavLink activeclassname="active" to="/ProfilePage">          
+                                        <button>
+                                            <PersonOutlineOutlinedIcon className='icon'/>
+                                            Profile
+                                        </button>
+                                    </NavLink>
+                                </li>
+
+                                {/* BELOW: This will allow for ONLY the ADMIN (based on rank from DB) to view the admin nav icon, and navigate onto the AdminPage */}
+                                {/* ipv 1 this will be a useState. useState update from axios call that's being plled from the user's ID */}
+                                {getAdminPermission(rank) ? (
+                            
+                                <li>       
+                                    <NavLink activeclassname="active" to="/AdminPage">           
+                                        <button>
+                                            <AdminPanelSettingsOutlinedIcon className='icon'/>
+                                            Admin
+                                        </button>
+                                    </NavLink>
+                                </li>
+                                ) : (
+                                    <span></span>
+                                )}
+                            </div>
+                        ) : (
+                            <span></span>
+                        )}
+                    </div>
+
+                    <div className="discordBlock">
+                        <h6>Join Our</h6>
+                        <h4>Discord Server</h4>
+
+                        <Button
+                            sx={{
+                            backgroundColor: "#FFFFFF",
+                            height: "42px",
+                            color: "#2b2b2b",
+                            fontWeight: "bold",
+                            borderRadius: "20px",
+                            marginTop: "20px",
+                            width: "140px",
+                            fontFamily: "Open Sans",
+                            marginLeft: "600px",
+                            "&:hover": {
+                                backgroundColor: "#F6F6FA",
+                            },
+                            }}
+                            variant="contained"
+                            backgroundColor="primary"
+                            onClick={goToDiscord}
+                        >
+                            Join
+                        </Button>
+                    </div>
+                </div>
             </div>
-            <h3>{username}</h3>
-            <h5>{rank} </h5>
-          </div>
-
-          <div className="Navigation">
-            {loggedIn ? (
-              <div className="list">
-                <li>
-                  <NavLink exact activeclassname="active" to="/FeedPage">
-                    <button>
-                      <HomeOutlinedIcon className="icon" />
-                      <div className="text">Feed</div>
-                    </button>
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink activeclassname="active" to="/QuestionsPage">
-                    <button>
-                      <QuestionAnswerOutlinedIcon className="icon" />
-                      <div className="text">Questions</div>
-                    </button>
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink activeclassname="active" to="/ProfilePage">
-                    <button>
-                      <PersonOutlineOutlinedIcon className="icon" />
-                      <div className="text">Profile</div>
-                    </button>
-                  </NavLink>
-                </li>
-
-                {/* BELOW: This will allow for ONLY the ADMIN (based on rank from DB) to view the admin nav icon, and navigate onto the AdminPage */}
-                {/* ipv 1 this will be a useState. useState update from axios call that's being plled from the user's ID */}
-                {getAdminPermission(rank) ? (
-                  <li>
-                    <NavLink activeclassname="active" to="/AdminPage">
-                      <button>
-                        <AdminPanelSettingsOutlinedIcon className="icon" />
-                        <div className="text">Admin</div>
-                      </button>
-                    </NavLink>
-                  </li>
-                ) : (
-                  <span></span>
-                )}
-              </div>
-            ) : (
-              <span></span>
-            )}
-          </div>
         </div>
-
-        <div className="discordBlock">
-          <h6>Join Our</h6>
-          <h4>Discord Server</h4>
-
-          <Button
-            sx={{
-              backgroundColor: "#FFFFFF",
-              height: "42px",
-              color: "#2b2b2b",
-              fontWeight: "bold",
-              borderRadius: "20px",
-              marginTop: "20px",
-              width: "140px",
-              fontFamily: "Open Sans",
-              marginLeft: "600px",
-              "&:hover": {
-                backgroundColor: "#F6F6FA",
-              },
-            }}
-            variant="contained"
-            backgroundColor="primary"
-            onClick={goToDiscord}
-          >
-            Join
-          </Button>
-        </div>
-      </div>
-    </div>
-  );
-};
+    );
+}
 
 export default Navigation;
